6e11725d32448a6cb5d0bf7d72b5c5cff7f1e8ff
Kasun Abaywardana <abaywardanakasun@gmail.com>
2025-12-18 23:03:06 +0800

    update: Update axios interceptor

--- auth-fe/src/api/authService.js
+++ auth-fe/src/api/authService.js
@@ -2,7 +2,7 @@ import api from './axios'
 
 export default {
   async login({ username, password }) {
-    const response = await api.post('/auth/login', { username, password })
+    const response = await api.post('/auth/login', { username, password }, { skipAuth: true })
     return response.data
   },
 }
